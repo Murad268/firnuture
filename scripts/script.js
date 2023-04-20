a4d10e332998e5d3cd2bfca3e823d02d2a333ac8
Murad <agamedov94@mail.ru>
2023-04-20 23:20:10 +0400

    cat1

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,30 @@ function openMenu(triggerSelector, menuSelector, activeClass, closeTrigger) {
 document.addEventListener("DOMContentLoaded", () => {
    openMenu(".header__hamburger", '.navbar__menu', 'navbar__menu__active', '.navbar__menu__exit');
 })
+
+
+
+$('.categories__wrapper').slick({
+   infinite: true,
+   slidesToShow: 3,
+   centerMode: false,
+   slidesToScroll: 1,
+
+   prevArrow: '<div class="slideCarRigt"><i class="fa fa-arrow-right" aria-hidden="true"></i></div>',
+   nextArrow: '<div class="slideCarLeft"><i class="fa fa-arrow-left" aria-hidden="true"></i></div>',
+   responsive: [{
+         breakpoint: 1180,
+         settings: {
+            slidesToShow: 2,
+            arrows: false
+         }
+      },
+      {
+         breakpoint: 560,
+         settings: {
+            slidesToShow: 1,
+            arrows: false
+         }
+      }
+   ]
+});
