fddd6a1974e7d58a028710f2a4639832cdb06e78
Murad <agamedov94@mail.ru>
2023-04-21 00:01:30 +0400

    sr

--- scripts/script.js
+++ scripts/script.js
@@ -12,11 +12,35 @@ function openMenu(triggerSelector, menuSelector, activeClass, closeTrigger) {
    })
 }
 
+function searchVisible(searchSelector, overlaySelector, closeSelector, activeClass, srSelector, srActive) {
+   const search = document.querySelector(searchSelector),
+         overlay = document.querySelector(overlaySelector),
+         close = document.querySelector(closeSelector),
+         sr = document.querySelector(srSelector);
+
+   search.addEventListener("click", () => {
+      overlay.classList.add(activeClass);
+      sr.classList.add(srActive);
+      document.body.style.overflow="hidden";
+      document.body.style.marginRight = "17px"
+   })
+   close.addEventListener("click", () => {
+      overlay.classList.remove(activeClass);
+      sr.classList.remove(srActive);
+      document.body.style.overflow="auto";
+      document.body.style.marginRight = "0px"
+   })
+}
 
 
 document.addEventListener("DOMContentLoaded", () => {
    openMenu(".header__hamburger", '.navbar__menu', 'navbar__menu__active', '.navbar__menu__exit');
+   searchVisible(".header__top__left__search", '.overlay', '.header__sr__exit', 'overlay__active', '.header__sr', 'header__sr__active');
+   searchVisible(".header__top__left__search__right", '.overlay', '.header__sr__exit', 'overlay__active', '.header__sr', 'header__sr__active')
 })
+
+
+
 
 
 
